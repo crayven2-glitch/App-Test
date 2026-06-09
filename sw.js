6a5fb65647c5364bcb6b91dd4d6bf535e17931ce
crayven2-glitch <crayven2@gmail.com>
2026-06-09 21:13:32 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -36,7 +36,7 @@ self.addEventListener('notificationclick', e => {
 });
 
 // ── Cache (PWA offline) ──
-const CACHE_NAME = 'devotion-app-v15';
+const CACHE_NAME = 'devotion-app-v16';
 const ASSETS = ['./', './index.html'];
 
 self.addEventListener('install', e => {
